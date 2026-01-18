7cc3f49bf8beed6e290e94a99e53c56a42600f1a
wanshazamirul <wanshazamirul@users.noreply.github.com>
2026-01-18 11:41:55 +0800

    Add project configuration files

---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        sfdc: {
+          blue: {
+            50: '#E5F3FA',
+            100: '#CCE8F5',
+            200: '#99D1EB',
+            300: '#66B9E1',
+            400: '#33A1D7',
+            500: '#00A1E0',
+            600: '#0070D2',
+            700: '#005FB2',
+            800: '#032D60',
+            900: '#022145',
+          }
+        }
+      }
+    },
+  },
+};
+export default config;
